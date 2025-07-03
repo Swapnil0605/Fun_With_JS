4f3b9ec106c5070d23d639c6abdd71213f25322b
Swapnil0605 <swapnilchikte79@gmail.com>
2025-07-03 19:55:51 +0530

    Add files via upload
    Episode 1 - The basics of variables and more

---
+++ 01_variable.js
@@ -0,0 +1,16 @@
+const accountId =144553
+let accountEmail = "Swapnil@gogle.com"
+var accountPass = "1234"
+accountCity ="Maharashtra"
+let accountstate
+//accountId = 10 // not allowed 
+
+
+accountEmail = "fam@gmail.com"
+accountPass = "1234654"
+accountCity ="Maharashtra"
+
+
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPass , accountCity , accountstate])
