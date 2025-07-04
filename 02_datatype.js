80b6b2ff5e97cf2bb1b82d4d7858d23c08157fcf
Swapnil0605 <swapnilchikte79@gmail.com>
2025-07-04 19:18:41 +0530

    Add files via upload

---
+++ 02_datatype.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+
+// alert (3+3) // we r using nodejs , 
+// not browser it is in document form , 
+// as in the engine of js is in the browser browser 
+
+//console.log(3+3) ; console.log("Swapnil") not readable so avoid
+//semi can be used or not ,depends
+
+// ---DATA TYPE---
+/* number - 2 to power of 53
+   bigint
+   string - ""
+   boolean - T/F
+   null - standlone value
+   undefined - when the value is not specified
+   symbol - unique
+   
+   object*/
+
+   console.log(typeof undefined);
+   console.log(typeof null);
+   console.log(typeof "Swapnil");
+   console.log(typeof 1332434);
+   console.log(typeof true);
+   
+   
