fb0fd95927b6352953095cb5e9261d3c9684eed0
Swapnil0605 <swapnilchikte79@gmail.com>
2025-07-05 14:33:38 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,90 @@
+let score = "45"
+let score1 = "45abc"
+let score2 = null
+let score3 = undefined
+
+//console.log(typeof score)
+//or
+//console.log(typeof (score));
+
+let valueinnumber = Number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+let valueinnumber1 = Number(score1)
+//console.log(typeof valueinnumber1);
+//console.log(valueinnumber1);
+
+let valueinnumber2 = Number(score2)
+//console.log(typeof valueinnumber2);
+//console.log(valueinnumber2);
+
+
+let valueinnumber3 = Number(score3)
+//console.log(typeof valueinnumber3);
+//console.log(valueinnumber3);
+
+
+/*"33" - 33
+  "33abc" - NaN (not a number)
+  true - 1 ;  false - 0*/
+
+let isloggedin = 1
+let isloggedin1 = 0
+let isloggedin2 = ""
+let isloggedin3 = "Swapnil"
+
+let booleanisloggedin = Boolean(isloggedin)
+//console.log(booleanisloggedin);
+
+
+let booleanisloggedin1 = Boolean(isloggedin1)
+//console.log(booleanisloggedin1);
+
+let booleanisloggedin2 = Boolean(isloggedin2)
+//console.log(booleanisloggedin2);
+
+let booleanisloggedin3 = Boolean(isloggedin3)
+// console.log(booleanisloggedin3);
+
+
+//  --NOTES--
+/* 1 - true; 0 - false
+"" - false
+"hitesh" - true */
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+// console.log(typeof stringnumber);
+// console.log(stringnumber);
+
+// ------------operations---------------
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+/*
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/3)
+console.log(2%3)
+*/
+
+let str1 = "hello"
+let str2 = " Swapnil"
+let str3 = str1 + str2
+
+console.log(str3)
+
+
+console.log("1" + (2+2));
+console.log(1 + "2"+ 2);
+console.log(1+2+"2");
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log();
